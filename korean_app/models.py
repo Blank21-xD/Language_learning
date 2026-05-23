8a8cf526baa3d69089ccc8747a6e767340684d2d
Blank21-xD <lama.safal21@gmail.com>
2026-05-23 16:41:23 +1000

    New models with new class

--- korean_app/models.py
+++ korean_app/models.py
@@ -1,50 +1,37 @@
 from django.db import models
 
-# 1. User Table (For Broken Access Control demo)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
-class AppUser(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    # Storing plaintext/MD5 for insecure mode, but we will discuss hashing later
-    password = models.CharField(max_length=255)
-    # The target flag for access control
+class Vocabulary(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    hangul = models.CharField(max_length=100)
+    romanization = models.CharField(max_length=100)
+    definition = models.TextField()
+
+    def __str__(self):
+        return self.hangul
+
+
+class UserProfilereview(models.Model):
+    username = models.CharField(max_length=100)
+    learning_goal = models.TextField()
     is_premium = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
 
-# 2. Korean Dictionary Table (For SQL Injection demo)
 
-
-class Dictionary(models.Model):
-    korean_word = models.CharField(max_length=100)
-    english_definition = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"{self.korean_word} -> {self.english_definition}"
-
-# 3. Community Forum Table (For XSS demo)
-
-
-class ForumPost(models.Model):
-    # Relates the post to our AppUser model
-    user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
-    content = models.TextField()  # Where the malicious <script> tag will live
+class GuestBookComment(models.Model):
+    author = models.CharField(max_length=100)
+    comment_text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Post by {self.user.username} at {self.created_at}"
-
-
-class SecurityLog(models.Model):
-    # What type of attack did we detect? (e.g., "SQL Injection Attempt", "XSS Payload")
-    attack_type = models.CharField(max_length=100)
-
-    # The exact dirty text the hacker typed into our input box
-    malicious_payload = models.TextField()
-
-    # The clock stamp showing exactly when they tried it
-    detected_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"🚨 ALERT: {self.attack_type} blocked at {self.detected_at}"
+        return self.author
--- korean_app/urls.py
+++ korean_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import dictionary_search
+
 
 urlpatterns = [
-    path('', dictionary_search, name='dictionary_search'),
+
 ]
