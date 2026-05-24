cedb2a15f4823c51f8ae900bfe9586f1e64c5f25
Blank21-xD <lama.safal21@gmail.com>
2026-05-24 23:07:10 +1000

    Added base.html and home.html using heritance

--- korean_app/urls.py
+++ korean_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
+    path('', views.home, name="home"),
+    path('greetings/', views.greetings, name="greetings"),
+    path('slang/', views.slang, name="slang"),
 
 ]
--- korean_app/views.py
+++ korean_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 from django.db import connection
 from .models import Category, Vocabulary, UserProfilereview, GuestBookComment
+
+
+def home(request):
+    return render(request, 'korean_app/home.html')
+
+
+def greetings(request):
+    return render(request, 'korean_app/greetings.html')
+
+
+def slang(request):
+    return render(request, 'korean_app/slang.html')
