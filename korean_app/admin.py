9d6e25792a5c244b87e47ef5b1fc60acf0590a31
Blank21-xD <lama.safal21@gmail.com>
2026-06-05 23:46:52 +1000

    Added admin config

--- korean_app/admin.py
+++ korean_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import GuestbookEntry
 
-# Register your models here.
+@admin.register(GuestbookEntry)
+class GuestbookEntryAdmin(admin.ModelAdmin):
+    # This displays these columns neatly in a table layout inside the panel
+    list_display = ('username', 'comment', 'created_at')
+    
+    # Adds a clickable search bar to quickly scan logs by user or text strings
+    search_fields = ('username', 'comment')
+    
+    # Adds a sidebar filter pane to sort logs by submission date
+    list_filter = ('created_at',)
